24952aafd6478151a4b55fda0e8efc5a36cac8c1
eyvind <eyvind@192.168.0.106>
2019-04-08 10:53:35 +0200

    sdfsf

--- Exercise_flights/src/java/FlightTicket.java
+++ Exercise_flights/src/java/FlightTicket.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import javax.faces.application.FacesMessage;
 
@@ -6,100 +7,98 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean
 public class FlightTicket {
-	
-	private String firstName;
-	private String lastName;
-	private String occupation;
-	private String destination;
-	private String travelClass;
-	private ArrayList<String> destinationOptions;
-	private ArrayList<String> travelClassOptions;
-	
-	//	CONSTRUCTOR
-	public FlightTicket(){
-            
-                FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Managed Bean Initialized", null);        
+
+    private String firstName;
+    private String lastName;
+    private String occupation;
+    private String destination;
+    private String travelClass;
+    final private ArrayList<String> destinationOptions;
+    final private ArrayList<String> travelClassOptions;
+
+    //	CONSTRUCTOR
+    public FlightTicket() {
+
+        FacesMessage facesMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Managed Bean Initialized", null);
         FacesContext.getCurrentInstance().addMessage(null, facesMsg);
-		
-		firstName = "First name";
-		lastName = "Last name";
-		occupation = "Private";
-		
-		destinationOptions = new ArrayList<>();
-		destinationOptions.add("Copenhagen");
-		destinationOptions.add("Barcelona");
-		destinationOptions.add("New York");
-		destinationOptions.add("Halifax");
-		destinationOptions.add("Hong Kong");
-		
-		travelClassOptions = new ArrayList<>();
-		travelClassOptions.add("First");
-		travelClassOptions.add("Business");
-		travelClassOptions.add("Economy");
-	}
 
-	//	GETTERS / SETTERS
-	
-	public String getFirstName() {
-		return firstName;
-	}
+        firstName = "First name";
+        lastName = "Last name";
+        occupation = "Private";
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+        destinationOptions = new ArrayList<>();
+        destinationOptions.add("Copenhagen");
+        destinationOptions.add("Barcelona");
+        destinationOptions.add("New York");
+        destinationOptions.add("Halifax");
+        destinationOptions.add("Hong Kong");
 
-	public String getLastName() {
-		return lastName;
-	}
+        travelClassOptions = new ArrayList<>();
+        travelClassOptions.add("First");
+        travelClassOptions.add("Business");
+        travelClassOptions.add("Economy");
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    //	GETTERS / SETTERS
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public String getOccupation() {
-		return occupation;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public void setOccupation(String occupation) {
-		this.occupation = occupation;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public String getDestination() {
-		return destination;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
+    public String getOccupation() {
+        return occupation;
+    }
 
-	public ArrayList<String> getDestinationOptions() {
-		return destinationOptions;
-	}
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
 
-	public String getTravelClass() {
-		return travelClass;
-	}
+    public String getDestination() {
+        return destination;
+    }
 
-	public void setTravelClass(String flightClass) {
-		this.travelClass = flightClass;
-	}
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 
-	public ArrayList<String> getTravelClassOptions() {
-		return travelClassOptions;
-	}
-        
-        public void newMessage()
-    {
-    if (getFirstName().equalsIgnoreCase("solna")){
-            FacesMessage javaTextMsg = new FacesMessage(FacesMessage.SEVERITY_INFO, 
-                "Good Job, that is the correct answer!", null);
+    public ArrayList<String> getDestinationOptions() {
+        return destinationOptions;
+    }
+
+    public String getTravelClass() {
+        return travelClass;
+    }
+
+    public void setTravelClass(String flightClass) {
+        this.travelClass = flightClass;
+    }
+
+    public ArrayList<String> getTravelClassOptions() {
+        return travelClassOptions;
+    }
+
+    public void newMessage() {
+        if (getFirstName().equalsIgnoreCase("solna")) {
+            FacesMessage javaTextMsg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+                    "Good Job, that is the correct answer!", null);
             FacesContext.getCurrentInstance().addMessage("guessForm:gText", javaTextMsg);
         } else {
-            FacesMessage javaTextMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, 
-                "Sorry, that is NOT the correct answer!", null);
+            FacesMessage javaTextMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Sorry, that is NOT the correct answer!", null);
             //lägger i meddelandet i nestade gText, som finna i formuläret guessForm
             FacesContext.getCurrentInstance().addMessage("guessForm:gText", javaTextMsg);
         }
     }
-	
+
 }
